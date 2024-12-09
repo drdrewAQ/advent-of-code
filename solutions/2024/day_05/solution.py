43f7074335dc4a8c615505ecc0d4392f07403d18
Drew Parker <andrew.parker@sandboxquantum.com>
2024-12-09 16:04:39 -0500

    yay topological sort

--- solutions/2024/day_05/solution.py
+++ solutions/2024/day_05/solution.py
@@ -13,59 +13,71 @@ class Solution(TextSolution):
     def part_1(self) -> int:
         prereq, orders = self.input.split('\n\n')
 
-        must_pre = {}
+        must_follow = {}
         for rule in prereq.split('\n'):
             pre, post = rule.split('|')
-            if pre not in must_pre:
-                must_pre[pre] = [post]
-            else:
-                must_pre[pre].append(post)
+            must_follow.setdefault(pre, set()).add(post)
 
         sum = 0
         for order in orders.split('\n'):
             pages = order.split(',')
-            middle = int(pages[int(len(pages)/2)])
-            while pages:
+            middle = int(pages[len(pages)//2])
+            while pages and middle:
                 this = pages.pop(0)
                 for p in pages:
-                    if p in must_pre and this in must_pre[p]:
+                    if p in must_follow and this in must_follow[p]:
                         middle = 0
             sum += middle
 
         return sum
 
-    # @answer(1234)
+    @answer(4828)
     def part_2(self) -> int:
         prereq, orders = self.input.split('\n\n')
 
-        must_pre = {}
+        must_follow = {}
         for rule in prereq.split('\n'):
             pre, post = rule.split('|')
-            if pre not in must_pre:
-                must_pre[pre] = [post]
-            else:
-                must_pre[pre].append(post)
+            must_follow.setdefault(pre, set()).add(post)
 
         sum = 0
         for order in orders.split('\n'):
-            print(f'starting {order}')
             pages = order.split(',')
+
+            # build the partial order topology
+            topo = {p: 0 for p in pages}
+            for p in topo:
+                for q in must_follow:
+                    if q in pages and p in must_follow[q]:
+                        topo[p] += 1
+
             in_order = True
             sorted = []
             while pages:
-                this = pages.pop(0)
-                for p in pages:
-                    if p in must_pre and this in must_pre[p]:
-                        print(f'{this} is not in order with {p}')
-                        in_order = False
-                        pages.insert(pages.index(p)+1, this)
-                        break
-                if this not in pages:
+                if in_order:
+                    this = pages.pop(0)
+                else:
+                    # we're not in order, so sort (take first 0-deg node)
+                    for i, p in enumerate(pages):
+                        if topo[p] == 0:
+                            break
+                    this = pages.pop(i)
+
+                # this has no remaining predecessors
+                if topo[this] == 0:
+                    # put it into sorted
                     sorted.append(this)
+                    # and remove it as a predecessor
+                    for p in pages:
+                        if p in must_follow[this]:
+                            topo[p] -= 1
+                else:
+                    # otherwise put node back and finish with sort
+                    in_order = False
+                    pages.insert(0, this)
             if not in_order:
-                middle = int(sorted[int(len(sorted)/2)])
-                print(f'{middle} found from {sorted}')
-                sum += middle
+                middle = sorted[len(sorted)//2]
+                sum += int(middle)
         return sum
 
     # @answer((1234, 4567))
